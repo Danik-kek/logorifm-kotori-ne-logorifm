102c38267cf422a3c7eaf02d6a40880e144f2005
Loli admin <Loli admin@DESKTOP-IUG32E2>
2024-10-14 22:11:41 +0300

    realise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("enter number");
-            string input +
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int n) && n >= 0)
+            {
+                long factorial = CalculateFactorial(n);
+                Console.WriteLine($"Факториал числа {n} равен  {factorial}");
+
+            }
+            else
+            {
+                Console.WriteLine("полложительное чмсло ввел ? ");
+            }
+        }
+        static long CalculateFactorial(int n)
+        {
+            long result = 1;
+            for (int i = 2; i < n; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
